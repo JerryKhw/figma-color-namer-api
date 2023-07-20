72689e4ac45404f2d1a3800d7d056c0c0e4ae557
JERRY <hwjameshw@gmail.com>
2023-07-20 09:44:08 +0900

    refactor: api 수정

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-import express from "express";
+import express, { json } from "express";
 import cors from "cors";
 import { initEnv } from "#util";
 
 import nearestColor from 'nearest-color';
 import colorNameList from 'color-name-list' assert { type: "json" };
+
+import { body, validationResult } from 'express-validator';
 
 const colors = colorNameList.reduce((o, { name, hex }) => Object.assign(o, { [name]: hex }), {});
 const nearest = nearestColor.from(colors);
@@ -12,43 +14,118 @@ const env = initEnv();
 
 const app = express();
 
+app.use(json());
 app.use(cors());
 
-app.get("/", (req, res) => {
-    if (!req.query.colors) {
-        res.send("figma-color-namer-api");
-        return;
-    }
+app.get("/", (_, res) => {
+    res.send("figma-color-namer-api");
+})
 
-    const colors = req.query.colors.split(',')
+app.post("/v1", body('colors').notEmpty().isArray(), body('colors.*.hex').notEmpty().isString(), body('colors.*.opacity').notEmpty().isNumeric(), (req, res) => {
+    const result = validationResult(req);
 
-    if (colors.lenght == 0) {
-        res.send("figma-color-namer-api");
-        return;
-    }
-
-    try {
-        res.send(
+    if (!result.isEmpty()) {
+        res.status(400).send(
             {
-                "message": "success",
-                "data": colors.map((color) => {
-                    const { name } = nearest(color)
+                "message": "bad_request",
+            }
+        );
+        return;
+    }
 
-                    return {
-                        "hex": color,
-                        "name": name,
+    let colors = [];
+
+    colors = req.body.colors.reduce((list, color) => {
+        if (
+            list.findIndex(
+                ({ hex, opacity }) => hex === color.hex && opacity === color.opacity
+            ) === -1
+        ) {
+            list.push(color);
+        }
+        return list;
+    }, []);
+
+    let newColors = [];
+
+    colors.forEach(color => {
+        try {
+            let { name } = nearest(color.hex)
+            while (true) {
+                const colorName = name.toLowerCase().replaceAll(" ", "_").replace("/", "");
+
+                const index = newColors.findIndex(({ hex, name }) => color.hex == hex && colorName == name)
+
+                if (index != -1) {
+                    if (color.opacity == 1) {
+                        newColors.push(
+                            {
+                                "hex": color.hex,
+                                "opacity": color.opacity,
+                                "name": colorName,
+                            }
+                        )
+                    } else {
+                        newColors.push(
+                            {
+                                "hex": color.hex,
+                                "opacity": color.opacity,
+                                "name": colorName + "_" + Math.round(color.opacity * 100) + "%",
+                            }
+                        )
                     }
-                })
+                    break;
+                } else {
+                    if (newColors.map((color) => color.name).includes(colorName)) {
+                        const names = name.split("/");
+
+                        if (names.length > 1) {
+                            name = names[0] + "/" + (parseInt(names[names.length - 1]) + 1);
+                        } else {
+                            name = names[0] + "/1";
+                        }
+                    } else {
+                        if (color.opacity == 1) {
+                            newColors.push(
+                                {
+                                    "hex": color.hex,
+                                    "opacity": color.opacity,
+                                    "name": colorName,
+                                }
+                            )
+                        } else {
+                            newColors.push(
+                                {
+                                    "hex": color.hex,
+                                    "opacity": color.opacity,
+                                    "name": colorName + "_" + Math.round(color.opacity * 100) + "%",
+                                }
+                            )
+                        }
+                        break;
+                    }
+                }
             }
-        );
-    } catch (e) {
-        res.send(
+        } catch (e) {
+            console.log(e);
+        }
+    });
+
+    if (newColors.length == 0) {
+        res.status(404).send(
             {
-                "message": "not_found_color",
+                "message": "not_found_colors",
             }
         );
+        return;
     }
 
+    res.status(200).send(
+        {
+            "message": "success",
+            "data": newColors,
+        }
+    );
 });
 
 app.listen(env.port);
